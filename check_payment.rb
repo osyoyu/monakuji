770f5801b491eaca9ec8d5ae9aaed2b3b538ced1
osyoyu <di.aritomo@gmail.com>
2014-01-27 02:17:50 +0900

    Format output of payment check script

--- check_payment.rb
+++ check_payment.rb
@@ -6,19 +6,21 @@ require './monacoin_rpc.rb'
 wallet = MonacoinRPC.new('http://monacoinrpc:E3P7qnDmbLsmvLTp7cyyLwJ4d1PZsr9WrVTBkBxR34jZ@127.0.0.1:10010')
 
 loop do
-
   Sheet.all.each do |sheet|
+    puts "Sheet #{sheet.name}"
+
     if !sheet.paid?
-      puts "Sheet #{sheet.name}"
-      p sheet.paid_confirmed = wallet.getreceivedbyaddress(sheet.address, 4) # 6 confs is too slow
-      p sheet.paid =  wallet.getreceivedbyaddress(sheet.address, 0)
-      
+      sheet.paid_confirmed = wallet.getreceivedbyaddress(sheet.address, 1)
+      sheet.paid =  wallet.getreceivedbyaddress(sheet.address, 0)
+
+      puts "Conf: #{sheet.paid_confirmed} / TTL: #{sheet.paid}"
       sheet.save
     else
       puts "Sheet #{sheet.name} is paid"
     end
   end
 
+  puts '========'
   sleep 15
 end
 
